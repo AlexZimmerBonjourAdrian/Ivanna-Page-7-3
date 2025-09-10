431cfb9b8532886abe9459073dfa17a3c2e7d22f
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-09-10 17:44:27 -0300

    New

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app.secret_key = os.environ.get("SESSION_SECRET", "storytelling-microtales-secre
 def index():
     return render_template('index.html')
 
-@app.route('/microtales')
+@app.route('/microtales/')
 def microtales():
     return render_template('microtales.html')
 
-@app.route('/story-formats')
+@app.route('/story-formats/')
 def story_formats():
     return render_template('story_formats.html')
 
-@app.route('/interactive-guide')
+@app.route('/interactive-guide/')
 def interactive_guide():
     return render_template('interactive_guide.html')
 
